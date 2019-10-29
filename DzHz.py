18dc59164e0d9ca8b7ab30a0e6acf6f1ae6b816f
Nikita <basynin.nikita@gmail.com>
2019-10-29 21:01:54 +0300

    hz

--- DzHz.py
+++ DzHz.py
@@ -1,15 +1,17 @@
 a = input('Введите массив\n')
-squares = list()
+squares = set()
 
 
+# Функция form из исходной строки с числами заданного массива
+# формирует матрицу смежности являющуюся входными данными
+# для задачи поиска гамильтонова пути
 def form(stroka):
     b = list()
     for i in stroka.split():
         b.append(int(i))
     b.sort()
     # squares = list()
-    for i in range(1, int((2 * b[len(b) - 1] - 1) ** 0.5) + 1):
-        squares.append(i ** 2)
+    squares = {i ** 2 for i in range(1, int((2 * b[len(b) - 1] - 1) ** 0.5) + 1)}
     matr = [[1 if i + j in squares and i != j else 0 for j in b] for i in b]
     print(b)
     print(squares)
