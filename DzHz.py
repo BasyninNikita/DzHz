a06f1aed61b8bab2e16c982167a20e5a7d141f27
Nikita <basynin.nikita@gmail.com>
2019-10-29 19:37:27 +0300

    hz

--- DzHz.py
+++ DzHz.py
@@ -8,14 +8,15 @@ def form(stroka):
         b.append(int(i))
     b.sort()
     # squares = list()
-    for i in range(1, int((2 * b[len(b)-1] - 1) ** 0.5) + 1):
+    for i in range(1, int((2 * b[len(b) - 1] - 1) ** 0.5) + 1):
         squares.append(i ** 2)
     matr = [[1 if i + j in squares and i != j else 0 for j in b] for i in b]
     print(b)
     print(squares)
     print(len(b))
-    for i in range (len(b) ):
-        print(matr [i])
+    for i in range(len(b)):
+        print(matr[i])
     return matr
+
 
 form(a)
